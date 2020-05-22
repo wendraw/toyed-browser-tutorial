fc6b799cd9d34d32257eba90d8b3d8d007b11081
wendraw <252798869@qq.com>
2020-05-22 15:34:09 +0800

    Step 1 request url

---
+++ servers/server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log("request receive");
+  res.setHeader("Content-Type", "text/html"); // html 格式浏览器会直接解析
+  res.setHeader("X-Foo", "bar");
+  res.writeHead(200, { "Content-Type": "text/plain" }); // plain 则只会认为是字符串
+  // 同步读取
+  var data = fs.readFileSync("./servers/index.html");
+  res.end(data.toString());
+});
+
+server.listen(8088);
